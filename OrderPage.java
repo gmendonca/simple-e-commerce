0fc0c04c5cf62c4b7cc74261d8a6b7650f509673
Gustavo Mendonca <guzzgm@gmail.com>
2014-10-09 18:21:13 -0500

    Missing Cancel Order Verification Date

--- OrderPage.java
+++ OrderPage.java
@@ -20,112 +20,25 @@ public class OrderPage extends HttpServlet {
 
     String orders = new String();
     if(session.getAttribute("orders") != null) orders = (String)session.getAttribute("orders");
-    
-    
-    response.setContentType("text/html");
-    
-    String docType = "<!DOCTYPE html>";
-    out.println(docType +
-        "<html>\n" +
-        "<head>\n" +
-        "<title>" + title + "</title>\n" +
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/styles.css\">" +
-        "</head>\n" +
-        "<body>\n" +
-        "<header>\n" +
-        "<table border=\"1\" width=\"100%\">\n" +
-        "<tr>\n" +
-        "<td width=\"80%\">\n" +
-        "<h1><a href=\"/ecom/HomePage\">Best Deal</a></h1>\n" +
-        "</td>\n" +
-        "<td width=\"20%\">\n" +
-        "<h4><a href=\"/ecom/CartPage\">Cart</a></h4>\n" +
-        "</td>\n" +
-        "</tr>\n" +
-        "</table>\n" +
-        "<table border=\"1\" width=\"100%\">\n" +
-        "<tr>\n" +
-        "<td width=\"40%\">\n" +
-        "</td>\n" +
-        "<td width=\"30%\">\n" +
-        "<a href=\"#\">Weekly Deals</a>\n" +
-        "</td>\n" +
-        "<td width=\"30%\">\n");
-    if(session.getAttribute("username") != null){
-        username = (String)session.getAttribute("username");
-        out.println("Hi, <a href=\"/ecom/OrderPage\">" + username + "</a>\n");
-    }else{
-        out.println("<a href=\"/ecom/SignInPage\">Sign in</a>\n");
-    }
-    out.println(
-        "</td>\n" +
-        "</tr>\n" +
-        "</table>\n" +
-        "</header>\n");
-    out.println(
-        "<nav>\n" +
-        "<form action=\"/ecom/CatalogPage\">\n" +
-        "<table>\n" +
-        "<tr>\n" +
-        "<td>\n" +
-        "<input id=\"button\" type=\"submit\" name =\"product\" VALUE=\"Phones\">\n" +
-        "</td>\n" +
-        "</tr>\n" +
-        "<tr>\n" +
-        "<td>\n" +
-        "<input id=\"button\" type=\"submit\" name =\"product\" VALUE=\"Tablets\">\n" +
-        "</td>\n" +
-        "</tr>\n" +
-        "<tr>\n" +
-        "<td>\n" +
-        "<input id=\"button\" type=\"submit\" name =\"product\" VALUE=\"Laptop\">\n" +
-        "</td>\n" +
-        "</tr>\n" +
-        "<tr>\n" +
-        "<td>\n" +
-        "<input id=\"button\" type=\"submit\" name =\"product\" VALUE=\"TV\">\n" +
-        "</td>\n" +
-        "</tr>\n" +
-        "</table>\n" +
-        "</form>\n" +
-        "</nav>\n" +
-        "<aside>\n" +
-        "<h1 align=\"center\">" + title + "</h1>");
 
-    if(orders.compareTo("") == 0){
-        out.println("You don't have any open order!");            
-    }else{
-        out.println("<form action=\"/ecom/OrderPage\" method=\"POST\">");
-        out.println("<table border=\"1\">");
+    cancel = request.getParameter("cancel");
+
+    if(cancel != null){
+        String[] justCancel = cancel.split("\\s+");
+
         String delims = "[|]";
         String[] tokens = orders.split(delims);
-        for (int i = 0; i < tokens.length; i++){
-            out.println("<tr>\n<td>");
-            out.println("Order " + i);
-            out.println("</td>\n<td>");
-            out.println(tokens[i]);
-            out.println("</td>\n<td>");
-            out.println("<input id=\"buybutton\" type=\"submit\" name =\"cancel" + tokens[i] + "\" VALUE=\"Cancel\">\n");
-            out.println("</td>\n</tr>");
+        orders = new String();
+        for(int i = 0; i < tokens.length; i++){
+            if(justCancel[1] == i - 1){
+                continue;
+            }else{
+                orders = new String(tokens[i] + "|" + orders); 
+            }
         }
-        out.println("</td>\n</tr>");
-        out.println("</table>");
+
+        session.setAttribute("orders", orders);
     }
-
-    out.println("</aside>");
-    out.println("</body>");
-    out.println("</html>");
-  }
-
-  public synchronized void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    PrintWriter out = response.getWriter();
-    title = "Orders";
-
-    HttpSession session = request.getSession(true);
-
-    String orders = new String();
-    if(session.getAttribute("orders") != null) orders = (String)session.getAttribute("orders");
     
     
     response.setContentType("text/html");
@@ -207,11 +120,11 @@ public class OrderPage extends HttpServlet {
         String[] tokens = orders.split(delims);
         for (int i = 0; i < tokens.length; i++){
             out.println("<tr>\n<td>");
-            out.println("Order " + i);
+            out.println("Order " + i + 1);
             out.println("</td>\n<td>");
             out.println(tokens[i]);
             out.println("</td>\n<td>");
-            out.println("<input id=\"buybutton\" type=\"submit\" name =\"cancel" + tokens[i] + "\" VALUE=\"Cancel\">\n");
+            out.println("<input id=\"buybutton\" type=\"submit\" name =\"cancel\" VALUE=\"Cancel " + i + 1 + "\">\n");
             out.println("</td>\n</tr>");
         }
         out.println("</td>\n</tr>");
